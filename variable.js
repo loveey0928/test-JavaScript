a0354faf7cc69f6c8a263f75a623cc6971e740f0
Joon <32371480+loveey0928@users.noreply.github.com>
2020-07-11 10:42:29 +0900

    windowsize변화 시에 windowsize 업데이트 연습

--- variable.js
+++ variable.js
@@ -12,11 +12,11 @@ console.log('Hello World!');
 const daysInWeek = 7;
 const maxNumber = 5;
 
-const Joon = { name: 'Joon', age: 28 };
+// const Joon = { name: 'Joon', age: 28 };
 
-Joon.age = 29;
+// Joon.age = 29;
 
-console.log(Joon);
+// console.log(Joon);
 
 // 4. Function declaration
 function PrintHello(message) {
@@ -31,4 +31,106 @@ PrintHello('holiday');
 // }
 
 //args.forEach( (arg) => console.log(arg) );
-//const add = (a,b) => a+b;
+// const add = (a, b) => a + b;
+
+// console.log(add(1, 2));
+
+//-------------------------'--------------------------------'--------------
+// let number = 1;
+
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   speak() {
+//     console.log('${    number} : asdf');
+//   }
+// }
+
+// const Joon = new Person('Joon', 28);
+
+// Joon.speak();
+//''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+// class User {
+//   constructor(firstName, lastName, age) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//   }
+//   get age() {
+//     return this._age;
+//   }
+//   set age(value) {
+//     this._age = value < 0 ? 0 : value;
+//   }
+
+//   speak() {
+//     console.log(this.age);
+//   }
+// }
+
+// //const Ho = new User('Hojoon', 'Yoon', 3);
+
+// //console.log(Ho.age);
+
+// class Someone1 extends User {
+//   speak() {
+//     super.speak();
+//     console.log('aaaa');
+//   }
+// }
+
+// const seo = new Someone1('a', 'b', 6);
+
+// //console.log(seo.age);
+
+// seo.speak();
+//---------------------------object-------------------------------------------------
+// 1. Literals and properties
+// object = { key : value};
+// const cat = { name: 'yayong', age: 4 };
+
+// console.log(cat.name, cat.age);
+// console.log('aa');
+// console.log(cat['name'], cat['age']);
+
+//
+// function MakePerson(name, age) {
+//   console.log(name, age);
+// }
+
+// MakePerson('Joon', 21);
+// console.clear();
+const arrs = [0, 1];
+
+// for (let arr of arrs) {
+//   console.log(arr);
+// }
+// console.clear();
+
+// arrs.forEach((arr, index) => console.log(arr, index));
+
+// arrs.push('5');
+
+// for (let arr of arrs) {
+//   console.log(arr);
+// }
+// //console.clear();
+
+// //arrs.pop();
+
+// for (let arr of arrs) {
+//   console.log(arr);
+// }
+
+// const s = 'abc,defg,hijkl,mnopq';
+// const arr2 = s.split(',', 4);
+// console.log(arr2);
+
+//----------------'---------------------------------'---------------------------------------
+
+const id = prompt('enter your id');
+const password = prompt(`enter your ${arrs[0]}`);
+console.log(id, password);
